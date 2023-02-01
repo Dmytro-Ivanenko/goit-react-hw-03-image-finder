3e4bf72db8f80331b23e1665649d9be0916655a1
Dmytro-Ivanenko <dmitro0399@gmail.com>
2023-02-01 18:52:44 +0200

    updated load more

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
+import { ColorRing } from 'react-loader-spinner';
+
 import Searchbar from './shared/components/Searchbar/Searchbar';
 import ImageGallery from './modules/ImageGallery/ImageGallery';
+import Button from './shared/components/Button/Button';
 
 import { searchImages } from './shared/services/pixabay-api';
 
@@ -23,7 +26,7 @@ class App extends Component {
 
   async fetchImages() {
     try {
-      this.setState({ loading: true });
+      this.setState({ loading: true }); // check is work it?
       const { search, page } = this.state;
 
       const data = await searchImages(search, page);
@@ -42,12 +45,36 @@ class App extends Component {
     this.setState({ search, images: [], page: 1 });
   };
 
+  loadMore = e => {
+    console.log(e);
+    this.setState(({ page }) => {
+      return { page: page + 1 };
+    });
+  };
+
   render() {
     const { images, loading, error, showModal } = this.state;
+    const { loadMore, handleSearchSubmit } = this;
     return (
       <>
-        <Searchbar onSubmit={this.handleSearchSubmit} />
+        <Searchbar onSubmit={handleSearchSubmit} />
         <ImageGallery images={images} />
+        {loading && (
+          <ColorRing
+            visible={true}
+            height="80"
+            width="80"
+            ariaLabel="blocks-loading"
+            wrapperStyle={{}}
+            wrapperClass="blocks-wrapper"
+            colors={['#e15b64', '#f47e60', '#f8b26a', '#abbd81', '#849b87']}
+          />
+        )}
+        {Boolean(images.length) && (
+          <Button type="button" onClickBtn={loadMore}>
+            Load more
+          </Button>
+        )}
       </>
     );
   }
--- src/shared/services/pixabay-api.js
+++ src/shared/services/pixabay-api.js
@@ -8,17 +8,12 @@ const instance = axios.create({
   },
 });
 
-export const searchImages = async (q, _page = 1) => {
+export const searchImages = async (q, page = 1) => {
   const { data } = await instance.get('/', {
     params: {
       q,
-      _page,
+      page,
     },
   });
-  return data;
-};
-
-export const getAllImages = async () => {
-  const { data } = await instance.get('/');
   return data;
 };
